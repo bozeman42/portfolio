b7f950c5680aec28e85fd4c3980c4c3b07177ac1
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2018-02-01 17:31:38 -0600

    api test

--- server/server.js
+++ server/server.js
@@ -2,6 +2,12 @@ let express = require('express');
 let app = express();
 const PORT = 5000;
 
+let postRouter = require('./routes/router.post');
+
 app.use(express.static('server/public'));
+
+// routes
+app.use('/api/post',postRouter);
+
 
 app.listen(PORT,() => console.log(`Listening on port ${PORT}...`))
