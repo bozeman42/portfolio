ff1a21af487c7b3107bc0d880753834f837c37f3
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2018-02-04 18:45:01 -0600

    writing post on action delegation

--- server/public/js/controllers/controller.post.js
+++ server/public/js/controllers/controller.post.js
@@ -1,18 +1,19 @@
 app.controller('PostController',function($location,BlogService){
   var vm = this;
+  vm.posts = BlogService.posts;
 
   vm.getPost = function() {
+    // read the post name from the address /#/post/[post-name]
     var address = location.hash.split('/')[2];
-    if (vm.posts[address]) {
-      vm.selectedPost = vm.posts[address];
-      console.log(vm.selectedPost)
-    } 
-    else {
+    // find the post with that that name
+    vm.selectedPost = vm.posts.find(function(post){
+      return (post.address == address);
+    });
+    // if the post isn't found, return to the blog page or a future /not-found page
+    if (!vm.selectedPost) {
       $location.path('/not-found');
     }
   }
 
-  vm.posts = BlogService.posts;
   vm.getPost();
-
 });
--- server/public/js/services/service.blog.js
+++ server/public/js/services/service.blog.js
@@ -3,13 +3,14 @@ app.service('BlogService', function () {
 
   var TEMPLATE_ADDR = '../views/partials/posts/';
 
-  function templateAddr(template){
+  function templateAddr(template) {
     return TEMPLATE_ADDR + template;
   }
 
   // TEMPLATE FOR POST OBJECT
   var postObjectTemplate = {
-    index: 12,
+    index: 12,  //int
+    address: '',
     heading: '',
     subheading: '',
     headerImage: 'img/post-bg.jpg',
@@ -22,26 +23,28 @@ app.service('BlogService', function () {
 
 
   // POSTS GO HERE
-  bs.posts = {
-    'GGJ2018': {
-      index: 2,
-      heading: "Global Game Jam 2018",
-      subheading: "Diving in to new technology.",
-      headerImage: 'img/ent-ro-py.jpg',
-      date: 'January 31, 2018',
-      author: 'Aaron Kvarnlov-Leverty',
-      tags: ['Games','c#','Unity','VR'],
-      template: templateAddr('GGJ2018.html')
-    },
-    'first-post': {
+  bs.posts = [
+    {
       index: 1,
+      address: 'first-post',
       heading: 'Hello, World!',
       subheading: 'Welcome to my tech blog!',
       headerImage: 'img/post-bg.jpg',
       date: 'January 31, 2018',
       author: 'Aaron Kvarnlov-Leverty',
-      tags: ['First post','non-technical'],
+      tags: ['First post', 'non-technical'],
       template: templateAddr('first-post.html')
+    },
+    {
+      index: 2,
+      address: 'GGJ2018',
+      heading: "Global Game Jam 2018",
+      subheading: "Diving in to new technology.",
+      headerImage: 'img/ent-ro-py.jpg',
+      date: 'January 31, 2018',
+      author: 'Aaron Kvarnlov-Leverty',
+      tags: ['Games', 'c#', 'Unity', 'VR'],
+      template: templateAddr('GGJ2018.html')
     }
-  }
+  ]
 });
