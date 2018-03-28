d237ca67288ab1854d1bf26ac8aa1fca820518d5
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2018-03-28 15:07:36 -0500

    changed link to one that will update content

--- server/public/js/controllers/controller.100.js
+++ server/public/js/controllers/controller.100.js
@@ -1,6 +1,6 @@
 app.controller('OneHundredDaysController',function($sce){
   var vm = this;
-  vm.logUrl = $sce.trustAsResourceUrl('https://rawgit.com/bozeman42/100-days-of-code/master/log.md');
+  vm.logUrl = $sce.trustAsResourceUrl('https://raw.githubusercontent.com/bozeman42/100-days-of-code/master/log.md');
   
   
 });
