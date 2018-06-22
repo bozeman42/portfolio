e5e6428f3f4941caf2742d3017777be0e5c7ef46
bozeman42 <bozeman42@gmail.com>
2018-06-22 10:29:37 -0500

    added resume

--- server/server.js
+++ server/server.js
@@ -7,7 +7,18 @@ let postRouter = require('./routes/router.post');
 app.use(express.static('server/public'));
 
 // routes
-app.use('/api/post',postRouter);
+app.use('/api/post', postRouter);
 
+function getOpenPort(app, PORT) {
 
-app.listen(PORT,() => console.log(`Listening on port ${PORT}...`));
+  app.listen(PORT, () => console.log(`Listening on port ${PORT}...`))
+    .on('error', (error) => {
+      console.log('error', error);
+      if (error.code === 'EADDRINUSE'){
+        PORT++;
+        getOpenPort(app, PORT);
+      }
+    })
+}
+
+getOpenPort(app, PORT);
